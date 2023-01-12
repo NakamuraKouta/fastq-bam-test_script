6c9b38bcd625f7732e97d711d6eac413bfaf2566
EC2 Default User <ec2-user@ip-172-31-3-20.ap-northeast-1.compute.internal>
2023-01-12 08:27:30 +0000

    test1_script.sh

--- test1_script.sh
+++ test1_script.sh
@@ -12,17 +12,15 @@ set -x
 
 sample1=$1
 
-#bwa mem でfastqアラインメント
-bwa/bwa mem Homo_sapiens_assembly38.fasta sequence1.fastq sequence2.fastq > ${sample1}.sam
 
-#samtoolsでsamからbamへ
-samtools-1.16.1/samtools view -bS ${sample1}.sam > ${sample1}.bam
+#bwa mem でfastqアラインメント
+bwa mem ./input/Homo_sapiens_assembly38.fasta ./input/sequence1.fastq ./input/seque
 
 #bamファイルのsort
-samtools-1.16.1/samtools sort -@4 ${sample1}.sam > ${sample1}.sort.bam
+samtools sort -@4 ${sample1}.sam > ${sample1}.sort.bam
 
 #baiファイルの作成
-samtools-1.16.1/samtools index ${sample1}.sort.bam
+samtools index ${sample1}.sort.bam
 
 #Duplicatesリードの除去
-gatk-4.2.0.0/gatk  MarkDuplicates   -I {$sample1}.sort.bam   -M metrics.txt   -O ${sample1}.MarkDup.bam --CREATE_INDEX
+gatk  MarkDuplicates   -I {$sample1}.sort.bam   -M metrics.txt   -O ${sample1}.MarkDup.bam --CREATE_INDEX
